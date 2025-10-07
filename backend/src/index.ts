b52cc3ac569ff3e29940a8ae64f59f51c45cef9c
samarth sharma <samarths716@gmail.com>
2025-10-07 14:39:00 +0530

    dev branch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 const app = express();
-
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello from samarth-sharma!');
 });
 
 app.listen(3000, () => {
