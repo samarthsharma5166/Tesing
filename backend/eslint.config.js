4a7c5a3f46b8443a3fa79baf53c84d88d2a34ab8
samarth sharma <samarths716@gmail.com>
2025-10-07 13:10:10 +0530

    main

---
+++ backend/eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      semi: 'error', //force semicolons
+      'no-unused-vars': 'warn', //warn if variable are unused
+    },
+  },
+];
